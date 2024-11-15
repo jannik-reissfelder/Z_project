efb9afb41f6cf8c90a4f67b174b58d29262fa668
jannik-reissfelder <jannik.reissfelder@gmail.com>
2024-11-15 13:53:35 +0100

    logo

--- app/app_pre.py
+++ app/app_pre.py
@@ -96,17 +96,17 @@ try:
     # List all files and directories in the specified path
     entries = os.listdir(directory_path)
     if entries:
-        print(f"Contents of '{directory_path}':")
+        st.write(f"Contents of '{directory_path}':")
         for entry in entries:
-            print(entry)
+            st.write(entry)
     else:
-        print(f"The directory '{directory_path}' is empty.")
+        st.write(f"The directory '{directory_path}' is empty.")
 except FileNotFoundError:
-    print(f"The directory '{directory_path}' does not exist.")
+    st.write(f"The directory '{directory_path}' does not exist.")
 except PermissionError:
-    print(f"Permission denied to access '{directory_path}'.")
+    st.write(f"Permission denied to access '{directory_path}'.")
 except Exception as e:
-    print(f"An error occurred: {e}")
+    st.write(f"An error occurred: {e}")
 
 
 # Define the new processing function within app.py
