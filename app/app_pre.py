4fa4348528124a08115c4952142ae7db837567cf
jannik-reissfelder <jannik.reissfelder@gmail.com>
2024-11-15 13:51:38 +0100

    logo

--- app/app_pre.py
+++ app/app_pre.py
@@ -89,7 +89,24 @@ st.write("Current working directory:", os.getcwd())
 display_logo("./images/logo_cat.webp", position_top=-30, position_right=10, width=80)
 
 
+# Define the directory path
+directory_path = '/mount/src/z_project/images/'
 
+try:
+    # List all files and directories in the specified path
+    entries = os.listdir(directory_path)
+    if entries:
+        print(f"Contents of '{directory_path}':")
+        for entry in entries:
+            print(entry)
+    else:
+        print(f"The directory '{directory_path}' is empty.")
+except FileNotFoundError:
+    print(f"The directory '{directory_path}' does not exist.")
+except PermissionError:
+    print(f"Permission denied to access '{directory_path}'.")
+except Exception as e:
+    print(f"An error occurred: {e}")
 
 
 # Define the new processing function within app.py
